31ccb546525692e3fd7e82cd2944a83c12121653
Miki Tebeka <miki@353solutions.com>
2024-05-01 17:16:58 +0300

    api change

--- main.go
+++ main.go
@@ -13,29 +13,25 @@ import (
 //go:generate go fmt quotes.go
 
 func main() {
-	var options struct {
-		query string
-		count int
-	}
+	var count int
 	flag.Usage = func() {
 		name := path.Base(os.Args[0])
 		fmt.Fprintf(os.Stderr, "usage: %s [options]\n", name)
 		fmt.Fprintln(os.Stderr, "Will print `count` random quotes.\nOptions:")
 		flag.PrintDefaults()
 	}
-	flag.StringVar(&options.query, "query", "", "query quotes")
-	flag.IntVar(&options.count, "count", 1, "number of results to return")
+	flag.IntVar(&count, "n", 1, "number of results to return")
 	flag.Parse()
 
-	if flag.NArg() != 0 {
+	if flag.NArg() > 1 {
 		fmt.Fprintf(os.Stderr, "error: wrong number of arguments\n")
 		os.Exit(1)
 	}
 
 	quotes := quoteDB
 
-	if options.query != "" {
-		query := strings.ToLower(options.query)
+	if flag.NArg() == 1 {
+		query := strings.ToLower(flag.Arg(0))
 		quotes = make([]string, 0, len(quoteDB))
 		for _, q := range quoteDB {
 			text := strings.ToLower(q)
@@ -43,18 +39,18 @@ func main() {
 				quotes = append(quotes, q)
 			}
 		}
-	}
 
-	if len(quotes) == 0 {
-		fmt.Fprintf(os.Stderr, "error: not quotes matching %q\n", options.query)
-		os.Exit(1)
+		if len(quotes) == 0 {
+			fmt.Fprintf(os.Stderr, "error: not quotes matching %q\n", query)
+			os.Exit(1)
+		}
 	}
 
 	rand.Shuffle(len(quotes), func(i, j int) {
 		quotes[i], quotes[j] = quotes[j], quotes[i]
 	})
 
-	n := min(options.count, len(quotes))
+	n := min(count, len(quotes))
 	for i := range n {
 		fmt.Println(quotes[i])
 		fmt.Println()
